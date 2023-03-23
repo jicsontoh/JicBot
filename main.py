f15df722ac1bd625fd603039555272d2f2955f81
jicsontoh <jicsontoh@gmail.com>
2023-03-23 23:54:51 +0800

    Add Help Command

--- main.py
+++ main.py
@@ -20,12 +20,19 @@ def start(update, context):
     context.bot.send_message(chat_id=chat_id, text="Hello! I am JicBot. How can I help you :)")
 
 
+def help(update, context):
+    chat_id = update.effective_chat.id
+    context.bot.send_message(chat_id=chat_id, text="Here are the available commands:"
+                                                   "/start")
+
+
 bot = Bot(token=os.environ["TOKEN"])
 
 dispatcher = Dispatcher(bot=bot, update_queue=None)
 dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 # run the start function when the user invokes the /start command
 dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler("help", help))
 
 
 @app.post("/")
