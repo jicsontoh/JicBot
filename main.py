699f032905ffcd63c95759ba238adca59df603e4
jicsontoh <jicsontoh@gmail.com>
2023-03-23 23:48:53 +0800

    Add Start Command

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask, request
 from werkzeug.wrappers import Response
 
 from telegram import Bot, Update
-from telegram.ext import Dispatcher, Filters, MessageHandler, CallbackContext
+from telegram.ext import Dispatcher, Filters, MessageHandler, CallbackContext, CommandHandler
 
 app = Flask(__name__)
 
@@ -14,10 +14,18 @@ def echo(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(update.message.text)
 
 
+# set up the introductory statement for the bot when the /start command is invoked
+def start(update, context):
+    chat_id = update.effective_chat.id
+    context.bot.send_message(chat_id=chat_id, text="Hello! I am JicBot. How can I help you :)")
+
+
 bot = Bot(token=os.environ["TOKEN"])
 
 dispatcher = Dispatcher(bot=bot, update_queue=None)
 dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+# run the start function when the user invokes the /start command
+dispatcher.add_handler(CommandHandler("start", start))
 
 
 @app.post("/")
