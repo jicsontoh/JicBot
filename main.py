7339ef4b6589b1a5ac3865305b1cb1c7910874a8
jicsontoh <jicsontoh@gmail.com>
2023-04-15 00:22:32 +0800

    Add decide function

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 import http
+import random
 
 import pymongo
 from flask import Flask, request
 from werkzeug.wrappers import Response
 
 
-from telegram import Bot, Update
+from telegram import Bot, Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Dispatcher, Filters, MessageHandler, CallbackContext, CommandHandler, Updater
 
 app = Flask(__name__)
@@ -29,10 +30,15 @@ def start(update, context):
 
 def help(update, context):
     chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text="Here are the available commands: "
-                                                   "/start"
+    context.bot.send_message(chat_id=chat_id, text="Here are the available commands: \n"
+                                                   "/start\n"
                                                    "/help")
-
+def decide(update, context):
+    chat_id = update.effective_chat.id
+    rand = random.randint(0, len(context.args) - 1)
+    res = "Hmmm we will go with "
+    res += context.args[rand]
+    context.bot.send_message(chat_id=chat_id, text=res)
 
 def test(update, context):
     chat_id = update.effective_chat.id
@@ -50,6 +56,7 @@ dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 dispatcher.add_handler(CommandHandler("start", start))
 dispatcher.add_handler(CommandHandler("help", help))
 dispatcher.add_handler(CommandHandler("test", test))
+dispatcher.add_handler(CommandHandler("decide", decide))
 
 # to test locally
 # updater.start_polling()
